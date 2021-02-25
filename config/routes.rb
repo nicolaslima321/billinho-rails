de77ffecab32766660b7d38fd0aabdb9a7fe4fdd
Nicolas Lima <nicolas.lima@redealumni.com>
2021-02-25 19:00:06 -0300

    Controllers Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'student/index'
+  get 'student/show'
+  get 'student/new'
+  get 'student/edit'
+  get 'invoice/index'
+  get 'invoice/show'
+  get 'invoice/new'
+  get 'invoice/edit'
+  get 'institution/index'
+  get 'institution/show'
+  get 'institution/new'
+  get 'institution/edit'
+  get 'enrollment/index'
+  get 'enrollment/show'
+  get 'enrollment/new'
+  get 'enrollment/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
